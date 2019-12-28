9d2922f5fc5159fa25e0bab0a8836a407a688d5f
Nathan Sherburn <nathansherburn@gmail.com>
2019-12-28 14:03:47 +1100

    Add basic server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+app = express();
+
+app.use(express.static('public'))
+app.get('*', function(req, res){
+  res.sendFile(__dirname + '/public/index.html')
+});
+app.listen(3000);
